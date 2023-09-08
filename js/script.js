21c75c14a258caeeb357b822ec4d3d063642d25b
VeronicaFontana <fontana.veronica98@gmail.com>
2023-09-08 18:37:05 +0200

    [FIX] Logic order

--- js/script.js
+++ js/script.js
@@ -1,26 +1,35 @@
-const nameLastName = document.getElementById("name-lastname").value;
-const km = parseInt(document.getElementById("km"));
-const age = document.getElementById("age");
+let nameLastName = document.getElementById("name-lastname");
+let km = document.getElementById("km");
+let age = document.querySelector("#age");
 
-const generaBtn = document.getElementById("genera");
-const annullaBtn = document.getElementById("annulla");
+let generaBtn = document.getElementById("genera");
+let annullaBtn = document.getElementById("annulla");
 
 
-const costoAlKm = km * 0.21;
-const costoAlKmDecimale = costoAlKm.toFixed(2);
 
-const costoBigliettoMinorenni = (costoAlKmDecimale - ((costoAlKmDecimale * 20) / 100)).toFixed(2);
-const costoBigliettoOver = (costoAlKmDecimale - ((costoAlKmDecimale * 40) / 100)).toFixed(2);
+
 
 let messaggio; 
+let minorenne = 1;
+let maggiorenne = 2;
+let over65 = 3;
 
 generaBtn.addEventListener("click", function(){
-  if (isNaN(km)){
+  console.log(age.value);
+  console.log(km.value);
+
+  const kmValue = km.value;
+  const costoAlKm = kmValue * 0.21;
+  const costoAlKmDecimale = costoAlKm.toFixed(2);
+  const costoBigliettoMinorenni = (costoAlKmDecimale - ((costoAlKmDecimale * 20) / 100)).toFixed(2);
+  const costoBigliettoOver = (costoAlKmDecimale - ((costoAlKmDecimale * 40) / 100)).toFixed(2);
+
+  if (isNaN(kmValue)){
     messaggio = `Inserire dei km validi`; 
-  } else if (age.value = 1){
-    messaggio = `Il tuo biglietto costava ${costoAlKmDecimale}€, ma con lo sconto sono ${costoBigliettoMinorenni}€`;
-  } else if (age.value = 3) {
-    messaggio = `Il tuo biglietto costava ${costoAlKmDecimale}€, ma con lo sconto sono ${costoBigliettoOver}€`;
+  } else if (age.value == minorenne){
+    messaggio = `Il tuo biglietto costava ${costoAlKmDecimale}€, ma con lo sconto sono ${costoBigliettoMinorenni}€ perché sei minorenne`;
+  } else if (age.value == over65){
+    messaggio = `Il tuo biglietto costava ${costoAlKmDecimale}€, ma con lo sconto sono ${costoBigliettoOver}€ perché sei Over65`;
   } else {
     messaggio = `Il tuo biglietto costa ${costoAlKmDecimale}€`
   }
